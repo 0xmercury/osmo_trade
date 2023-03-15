36e51f1a8c3141ae1834630df9052a3ca695300f
0xmercury <osmocosmo2@gmail.com>
2023-03-15 18:53:28 +0000

    update

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    readme = f.read()
+
 setup(
     name='osmo_trade',
-    version='0.1.0',    
+    version='0.1.1',
     description='SDK to trade on osmosis DEX',
     url='https://github.com/0xmercury/osmo_trade',
+    long_description= readme,
+    long_description_content_type="text/markdown",
     author='0xmercury',
     author_email='osmocosmo2@gmail.com',
     license='Apache License 2.0',
@@ -14,9 +19,12 @@ setup(
     },
 
     classifiers=[
-        'Intended Audience :: Trading',
-        'License :: OSI Approved :: Apache License',
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.9',
     ],
+    project_urls={
+        "Source": "https://github.com/0xmercury/osmo_trade",
+    }
 )
